da19b9d5f7f0d77587fef2f4ae390f7464c999e6
Kamil Surma <kamil.surma@wp.pl>
2018-12-11 12:49:37 +0100

    Dodanie pluginu surfire

--- src/test/java/kalkulator/KalkultorAppTest.java
+++ src/test/java/kalkulator/KalkultorAppTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Kocurek")
 class KalkultorAppTest {
@@ -17,13 +16,14 @@ class KalkultorAppTest {
         int suma = kalkultorApp.add(1, 13);
 
 //assert
-        Assertions.assertEquals(14, suma, "Sprawdź, że dodawanie liczb całkowitych dzialą");
+        Assertions.assertEquals(14, suma, "Sprawdź, że działa  dodawanie liczb całkowitych");
 
     }
 
     @Test
     public void addTwoPositiveDouble() {
         // arrange
+
         KalkultorApp kalkultorApp = new KalkultorApp();
 // act
         double suma = kalkultorApp.add(1.23, 3.0);
@@ -31,7 +31,15 @@ class KalkultorAppTest {
 //assert
         Assertions.assertEquals(4.23, suma, "Sprawdź, że dodawanie liczb całkowitych dzialą");
 
-        Assertions.assertEquals(4.23, suma, 0.0000001,  "Sprawdź, że dodawanie liczb całkowitych dzialą");
+        Assertions.assertEquals(4.23, suma, 0.0000001,  "Sprawdź, że działa  dodawanie liczb zmiennoprzecinkowych");
+    }
+
+    @Test
+    public void positiveDiv() {
+
+        KalkultorApp kalkultorApp = new KalkultorApp();
+        double suma =  kalkultorApp.div(2.0 , 3.0) ;
+        Assertions.assertEquals(0.66667, suma, 0.00001,  "Sprawdź, że dziala dzielenie liczb double");
     }
 
 }
